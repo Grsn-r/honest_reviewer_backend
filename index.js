ced7ac2c67cd67b419b37fa7f88b7cca4b3e0b5a
grsn_ <gers.e02@gmail.com>
2026-04-14 15:01:00 -0600

    initiated

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+const {PORT = 3001} = process.env;
+
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`server ejecutandose en ${PORT}`);
+})
