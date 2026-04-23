8522eedd912ad4551036392f98653725f7886699
Gerson <gers.e02@gmail.com>
2026-04-23 15:20:45 -0600

    Change login response message to 'login exitoso'

--- controllers/users.js
+++ controllers/users.js
@@ -36,7 +36,7 @@ const login = (req, res, next) => {
         .then(match => {
             if (match) {
                 const token = jwt.sign({_id: user._id}, process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'clave super secreta', {expiresIn: '7d'});
-                return res.send({token});
+                return res.send({message: 'login exitoso'});
             }
             throw new authError('contraseña incorrecta');
         })
